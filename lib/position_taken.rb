0cdb74a43761bac7dd3e535341ae6a90a95bcccd
Jacob Borga <jdborga@gmail.com>
2021-03-04 02:40:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
+  if board[index].strip == "X" || board[index].strip == "O"
+    return false
+  return true
 end
